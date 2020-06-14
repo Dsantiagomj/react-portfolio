1644e5fbb261481c3c9e3519c31fcf19c8524a30
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2020-06-14 17:41:38 -0500

    eslint and prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-nested-ternary': 'off',
+    'jsx-a11y/href-no-hash': ['off'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'max-len': [
+      'warn',
+      {
+        code: 80,
+        tabWidth: 2,
+        comments: 80,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'no-use-before-define': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+  },
+};
